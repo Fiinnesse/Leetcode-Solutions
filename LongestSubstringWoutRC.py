45858f4b54bd62369f2b6690b491fdcbe95ac423
FiinnLaptop <103233487+Fiinnesse@users.noreply.github.com>
2023-08-22 08:39:18 -0500

    Updated Changes

--- LongestSubstringWoutRC.py
+++ LongestSubstringWoutRC.py
@@ -1,7 +1,45 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         
+        st = ''
+        '''
+        p = ''
+        for x in s:
+            if st == '':
+                st += x
+                print("added")
+                p = x
+            elif x in s and x not in st:
+                print("true")
+                st += x
+                p = x
+            elif x in s and x in st and x == p:
+                st = x
+            else:
+                print("false")
+        '''
+
+        list=[]
+
+        for x in s:
+            if x not in list:
+                list.append(x)
+                st += x
+            elif x in list:
+                list.append(x)
+                i = list.pop()
+                list.append(i)
+        newst = ''
+        if st not in s:
+            list.pop(0)
+            newst = st[1:]
+            print(list)
+            return len(newst)
         
+
+            
+        #print(list)
+        #return st
 
 
 
