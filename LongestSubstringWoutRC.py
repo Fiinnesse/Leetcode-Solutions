787bc39399fbc3b84e868b23f1a985cc25086cf3
FiinnLaptop <103233487+Fiinnesse@users.noreply.github.com>
2023-08-21 23:02:34 -0500

    changes

--- LongestSubstringWoutRC.py
+++ LongestSubstringWoutRC.py
@@ -1,13 +1,7 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-    
-        st =  ''
-        for x in s[1::]:
-            if x not in st:
-            
-                st += x
-
-        return len(st)
+        
+        
 
 
 
@@ -16,5 +10,5 @@ s2 = 'bbbbb'
 s3 = 'pwwkew'
 s4 = " "
 sol = Solution()
-sollen = sol.lengthOfLongestSubstring(s4)
+sollen = sol.lengthOfLongestSubstring(s)
 print(sollen)
